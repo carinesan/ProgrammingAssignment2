d3dfc88c541e565a84fde71460701036510de67c
carinesan <carinesan93@gmail.com>
2014-05-18 22:44:21 +0200

    Assignment2 finished

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
-
+##makeCacheMatrix creates a special "matrix" object that can cache its inverse in four steps :
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse of the matrix
+## 4. get the value of the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y){
@@ -10,32 +12,42 @@ makeCacheMatrix <- function(x = matrix()) {
                 inv <<- NULL
         }
         get <- function() x
-        setinv <- function(solve) inv <<- inv
+        setinv <- function(solve) inv <<- solve
         getinv <- function() inv
         list (set = set, get = get,
               setinv = setinv,
               getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-## Return a matrix that is the inverse of 'x'
+##cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+##If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         inv <- x$getinv()
         if(!is.null(inv)) {
                 message("getting cached data")
                 return(inv)
-                }
+        }
         data <- x$get()
         inv <- solve(data, ...)
         x$setinv(inv)
         inv
 }
 
-b <- makeCacheMatrix()
-b$set(matrix(c(1,0,5,2,1,6,3,4,0),3,3))
-b$get
-cacheSolve(b)
-cacheSolve(b)
-matrix(c(1,0,5,2,1,6,3,4,0),3,3) %*% (cacheSolve(b))
+# b <- makeCacheMatrix()
+# b$set(matrix(c(1,0,5,2,1,6,3,4,0),3,3))
+# b$get
+# cacheSolve(b)
+# cacheSolve(b)
+# matrix(c(1,0,5,2,1,6,3,4,0),3,3) %*% (cacheSolve(b))
+
+amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+amatrix$get()         # Returns original matrix
+cacheSolve(amatrix)   # Computes, caches, and returns    matrix inverse
+amatrix$getinv()  # Returns matrix inverse
+cacheSolve(amatrix)   # Returns cached matrix inverse using previously computed matrix inverse
+
+amatrix$set(matrix(c(0,5,99,66), nrow=2, ncol=2)) # Modify existing matrix
+cacheSolve(amatrix)   # Computes, caches, and returns new matrix inverse
+amatrix$get()         # Returns matrix
+amatrix$getinv()  # Returns matrix inverse
